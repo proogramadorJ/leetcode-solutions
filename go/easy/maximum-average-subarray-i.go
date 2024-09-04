5fd16db9170a08d86e861bf1328f515184be424f
Pedro <pedroenxadrista1@gmail.com>
2024-09-04 18:23:35 -0300

    another easy sliding widown problem

---
+++ go/easy/maximum-average-subarray-i.go
@@ -0,0 +1,16 @@
+func findMaxAverage(nums []int, k int) float64 {
+    var ans, curr float64
+    
+    for i := 0; i < k; i++{
+        curr += float64(nums[i])
+    }
+    curr = curr
+    ans = curr 
+    
+    for i := k; i < len(nums); i++{
+        curr += float64(nums[i]  - nums[i - k])
+        ans = math.Max(curr, ans) 
+
+    }
+    return ans/float64(k);
+}
