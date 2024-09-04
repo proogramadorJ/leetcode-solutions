2fd5cd423346932182f8d2f4a971a4e3ae539093
Pedro <pedroenxadrista1@gmail.com>
2024-09-04 15:27:18 -0300

    sliding windown  problem

---
+++ go/medium/subarray-product-less-than-k.go
@@ -0,0 +1,17 @@
+func numSubarrayProductLessThanK(nums []int, k int) int {
+    if k <= 1 {
+        return 0
+    } 
+
+    var ans, left, curr = 0,0,1
+    for right :=0; right < len(nums); right++{
+        curr = curr * nums[right]
+        for curr >= k {
+            curr = curr / nums[left]
+            left++
+        }
+        ans = ans + right - left + 1
+    }
+
+    return ans
+}
